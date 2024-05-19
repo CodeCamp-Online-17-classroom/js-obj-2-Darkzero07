b7aaf63f18461066a39925e198562d87d6f0f86f
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-19 21:17:03 +0700

    js lab 48

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// write code here
+const user = {
+  email: 'cc@gmail.com',
+  isActive: true
+};
+
+user.isActive = false;
+console.log(user);      //{ email: 'cc@gmail.com', isActive: false } isActive has changed value
+
+
+
+user = {};
+console.log(user);          // Error user already declare
+
+
